8ab9c504e6733a33604c8abc34c5395ebc2cb88b
Astro <astro@spaceboyz.net>
2019-12-23 21:39:10 +0100

    publish.sh: update

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-set -x
+set -x -e
 
+make clean
 make
 git branch -D gh-pages
 git checkout -b gh-pages origin/master
-git add -f index.html
+git add -f ?.html
 git commit -m 'publish'
 git push -f
 git checkout master
